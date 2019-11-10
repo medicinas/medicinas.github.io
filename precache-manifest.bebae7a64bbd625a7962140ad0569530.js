159ad132769355c113cc605bc3cefe3f1b6677c4
sebastianbogado <sebastian.bogado@kimos.io>
2019-11-10 12:10:52 -0300

    Updates

---
+++ precache-manifest.bebae7a64bbd625a7962140ad0569530.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10fcda3ec60390a0d3900da9741d893b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25c6a34c10d2db8bbdc1",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6ebbb942b397e3de01b9",
+    "url": "/static/js/2.c3f44026.chunk.js"
+  },
+  {
+    "revision": "25c6a34c10d2db8bbdc1",
+    "url": "/static/js/main.8f8c66aa.chunk.js"
+  },
+  {
+    "revision": "c54781049104ca639b1b",
+    "url": "/static/js/runtime-main.1cf41bbe.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
